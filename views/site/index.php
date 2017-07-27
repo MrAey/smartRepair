380b66222ed4506e08c0493ae2091ab5c23f2fb3
Tharaphong Chamnan <admphrao@gmail.com>
2017-07-27 17:36:15 +0700

    edit site/index

--- views/site/index.php
+++ views/site/index.php
@@ -2,6 +2,55 @@
 
 /* @var $this yii\web\View */
 
+$this->title = Yii::$app->name;
+?>
+<div class="site-index">
+
+<div class="row">
+     <div class="col-lg-12">
+             <h1 class="page-header"><i class="fa fa-home fa-fw"></i> หน้าแรก</h1>
+     </div>        
+</div>
+
+<div class="row">
+            <div class="col-lg-3 col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-edit fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9 text-right">
+                                <div class="huge">0</div>
+                                <div>ใบส่งซ่อม ทั้งหมด</div>
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-tasks fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9 text-right">
+                                <div class="huge">0</div>
+                                <div>ใบส่งซ่อม วันนี้</div>
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+</div>
+</div>
+<?php
+
+/* @var $this yii\web\View */
+
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
